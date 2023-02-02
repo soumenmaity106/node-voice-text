012f7b08906c46da287e4a497eba4576874ca147
soumen <soumenmaity106@gmail.com>
2023-02-02 16:57:22 +0530

    add router

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
-
+const path = require("path");
 const app = express();
 
 app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
